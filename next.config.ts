40b981ea59d59f1612f178326fcc1c594ba33f7c
Francis <f.frechette@webitinteractive.ca>
2026-04-04 15:27:28 -0400

    fix: add turbopack config + migrate to correct Git repo

--- next.config.ts
+++ next.config.ts
@@ -1,13 +1,8 @@
 import type { NextConfig } from "next";
-import withPWAInit from "next-pwa";
-
-const withPWA = withPWAInit({
-  dest: "public",
-  disable: process.env.NODE_ENV === "development",
-});
 
 const nextConfig: NextConfig = {
   reactStrictMode: true,
+  turbopack: {},
 };
 
-export default withPWA(nextConfig);
+export default nextConfig;
