bdd53148892751415c45d2b2a09a3707dfaa0825
Francis <f.frechette@webitinteractive.ca>
2026-04-06 10:29:50 -0400

    feat: complete PWA metadata

--- app/layout.tsx
+++ app/layout.tsx
@@ -24,6 +24,13 @@ export const metadata: Metadata = {
     statusBarStyle: "default",
     title: "Gym Tracker",
   },
+  icons: {
+    apple: "/icons/apple-touch-icon.png",
+    icon: [
+      { url: "/icons/icon-192.png", sizes: "192x192", type: "image/png" },
+      { url: "/icons/icon-512.png", sizes: "512x512", type: "image/png" },
+    ],
+  },
 };
 
 export const viewport: Viewport = {
